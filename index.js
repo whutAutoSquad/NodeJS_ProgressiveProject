675c022f8185fd0c3445644ba6d0ccd34ac54ca5
liyf7218 <liyf7218@163.com>
2018-11-06 18:14:07 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(7002, () => console.log('Example app listening on port 7002!'))
