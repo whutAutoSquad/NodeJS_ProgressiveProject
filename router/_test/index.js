8b1ba8c5737331669d4840ea7a583c1279eb7aed
liyf7218 <liyf7218@163.com>
2018-12-12 17:14:09 +0800

    整理文件夹

--- router/_test/index.js
+++ router/_test/index.js
@@ -1,33 +1,35 @@
+/*
+* 此文件用于测试最新编写的接口
+* 当接口测试完成,或可用时,将完成部分移动到对应文件中
+*/
+
 import Router from 'koa-router'
 let router = new Router()
 
 router
   .post('/find', async (ctx, next) => {
     let reqBody = ctx.getValidatedProps(ctx.request.body)
+    console.log("reqBody: ", reqBody)
     ctx.body = await ctx.db.test.find(reqBody)
-    ctx.status = 200
   })
   .post('/create', async (ctx, next) => {
     let reqBody = ctx.getValidatedProps(ctx.request.body)
     console.log("reqBody: ", reqBody)
     ctx.body = await ctx.db.test.create(reqBody)
-
-    console.log(ctx.body)
-    ctx.status = 200
   })
   .post('/remove', async (ctx, next) => {
     let conditions = ctx.getValidatedProps(ctx.request.body)
+    console.log("conditions: ", conditions)
     ctx.body = await ctx.db.test.remove(conditions)
-    ctx.status = 200
   })
   .post('/update', async (ctx, next) => {
     let reqBody = ctx.getValidatedProps(ctx.request.body)
+    console.log("reqBody: ", reqBody)
     let conditions = { index: reqBody.index }
     let updates = {
       $set: reqBody
     }
     ctx.body = await ctx.db.test.update(conditions, updates)
-    ctx.status = 200
   })
 
 module.exports = router;
